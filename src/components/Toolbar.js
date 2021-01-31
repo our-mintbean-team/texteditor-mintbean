bb6e35544aaa22561b2c0a9b4f1907cbfb4b383f
danzhaas <dan.z.haas@gmail.com>
2021-01-31 16:44:41 -0500

    MVP buttons

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,46 +1,64 @@
-
 import React from "react";
 import { ButtonGroup, Button, DropdownButton, Dropdown } from "react-bootstrap";
 import {
+  FaFont,
   FaPalette,
   FaHighlighter,
   FaAlignLeft,
   FaAlignRight,
   FaAlignCenter,
   FaAlignJustify,
+  FaLink
 } from "react-icons/fa";
+import {
+  AiOutlineFontSize
+} from 'react-icons/ai';
 export default function Toolbar() {
   return (
     <ButtonGroup vertical id="toolbar">
+      <Button variant="secondary">
+        <FaFont />
+      </Button>
+      <Button variant="secondary">
+        <AiOutlineFontSize />
+      </Button>
       <Button variant="secondary">B</Button>
       <Button variant="secondary">I</Button>
       <Button variant="secondary">U</Button>
-      <Button variant="secondary">12</Button>
+      {/* <Button variant="secondary">12</Button> */}
 
-      <DropdownButton
-        as={ButtonGroup}
-        title={FaAlignLeft}
-        variant="secondary"
-        id="bg-nested-dropdown"
-      >
-        <Dropdown.Item eventKey="1">
+      <Dropdown as={ButtonGroup}>
+        <Button 
+          variant="secondary"
+          id="bg-nested-dropdown"
+          >
           <FaAlignLeft />
-        </Dropdown.Item>
-        <Dropdown.Item eventKey="2">
-          <FaAlignCenter />
-        </Dropdown.Item>
-        <Dropdown.Item eventKey="3">
-          <FaAlignRight />
-        </Dropdown.Item>
-        <Dropdown.Item eventKey="4">
-          <FaAlignJustify />
-        </Dropdown.Item>
-      </DropdownButton>
+        </Button>
+        <Dropdown.Toggle split variant="secondary" id="dropdown-split-basic" />
+        <Dropdown.Menu>
+          <Dropdown.Item eventKey="1">
+            <FaAlignLeft />
+          </Dropdown.Item>
+          <Dropdown.Item eventKey="2">
+            <FaAlignCenter />
+          </Dropdown.Item>
+          <Dropdown.Item eventKey="3">
+            <FaAlignRight />
+          </Dropdown.Item>
+          <Dropdown.Item eventKey="4">
+            <FaAlignJustify />
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+
       <Button variant="secondary">
         <FaPalette />
       </Button>
-      <Button variant="secondary">
+      {/* <Button variant="secondary">
         <FaHighlighter />
+      </Button> */}
+      <Button variant="secondary">
+        <FaLink />
       </Button>
     </ButtonGroup>
   );
