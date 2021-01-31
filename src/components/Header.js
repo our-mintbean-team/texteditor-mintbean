ac80eb4af45f857d2d24499830dc0c9b36597178
danzhaas <dan.z.haas@gmail.com>
2021-01-31 02:03:42 -0500

    improved header and styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 import ContentEditable from 'react-contenteditable'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee, faFileWord, faUserAstronaut } from '@fortawesome/free-solid-svg-icons'
+import { faICursor, faFileWord, faUserAstronaut } from '@fortawesome/free-solid-svg-icons'
 
 class DocNameEditor extends React.Component {
     constructor() {
@@ -28,21 +28,12 @@ export default function Header() {
 
     return (
         <div id='header'>
-            <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="#home"><FontAwesomeIcon icon={faCoffee} />Brand Name</Navbar.Brand>
-                
-                <Nav className="mr-auto">
-                <FontAwesomeIcon icon={faFileWord} /><NavDropdown title="Documents" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.4">New Doc</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.1">Docname1</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Docname2</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Docname3</NavDropdown.Item>
-                    </NavDropdown>
-                    <DocNameEditor />
-                    <Nav.Link href="#features"><FontAwesomeIcon icon={faUserAstronaut} />Login</Nav.Link>
-                </Nav>
-            </Navbar>
+            <div>
+                <Button variant="dark"><FontAwesomeIcon icon={faICursor} /> Editor Name</Button>
+                <Button variant="dark"><FontAwesomeIcon icon={faFileWord} /> Documents</Button>
+            </div>
+            <DocNameEditor />
+            <Button variant="dark"><FontAwesomeIcon icon={faUserAstronaut} /> Login</Button>
         </div>
     )
 }
