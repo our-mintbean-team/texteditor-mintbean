76ca9e5bbc09bed574bb29a2ac81ab68b7b3a991
DJ Pringle <n305oul93@gmail.com>
2021-01-31 12:43:30 -0600

    Updates initial structure of landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from 'react';
+import Header from '../components/Header';
+import { Button, ButtonGroup, Navbar } from 'react-bootstrap';
+import WalkingBook from '../images/walking-book.jpeg';
 
 function LandingPage() {
-    return (
-        <div>
-            <h2>LandingPage</h2>
+  return (
+    <div>
+      <Navbar bg='dark' variant='dark'>
+        <Navbar.Brand>Editor Name</Navbar.Brand>
+      </Navbar>
+
+      <div className='row'>
+        <div className='col-md-5'>
+          <img src={WalkingBook} alt='walking book' />
         </div>
-    )
+        <div className='col-md-5'>
+          <h1>Editor Name</h1>
+          <h3>The open source editor for all</h3>
+          <ButtonGroup>
+            <Button>Get Sarted</Button>
+            <Button>Change Theme</Button>
+          </ButtonGroup>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
