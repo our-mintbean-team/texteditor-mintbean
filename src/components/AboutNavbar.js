1fe291bb154d9e33e3f5ef4188e258a4d32e31b5
zaycation.dev <isaiahthomas098@gmail.com>
2021-01-31 23:38:29 -0800

    added working links

--- src/components/AboutNavbar.js
+++ src/components/AboutNavbar.js
@@ -1,29 +1,8 @@
 import React from "react";
-import ContentEditable from "react-contenteditable";
 import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 import { FaTextWidth, FaFileAlt, FaUserAstronaut } from "react-icons/fa";
 
-class DocNameEditor extends React.Component {
-  constructor() {
-    super();
-    this.state = { html: "UNTITLED DOCUMENT" };
-  }
-  handleChange = (evt) => {
-    this.setState({ html: evt.target.value });
-  };
-  render = () => {
-    return (
-      <ContentEditable
-        id="doc-name"
-        html={this.state.html} // innerHTML of the editable div
-        disabled={false} // use true to disable edition
-        onChange={this.handleChange} // handle innerHTML change
-      />
-    );
-  };
-}
-
-export default function Header() {
+export default function AboutNavbar() {
   return (
     <Navbar bg="dark" variant="dark" expand="md" className="navbar__container">
       <Navbar.Brand href="/">
@@ -33,6 +12,9 @@ export default function Header() {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
+          <Nav.Link href="/">
+            Live Editor
+          </Nav.Link>
           <NavDropdown title="Recent Docs" id="basic-nav-dropdown">
             <NavDropdown.Item href="/">New Doc</NavDropdown.Item>
             <NavDropdown.Divider />
@@ -41,7 +23,6 @@ export default function Header() {
             <NavDropdown.Item href="/">Docname3</NavDropdown.Item>
           </NavDropdown>
         </Nav>
-        <DocNameEditor />
         <Nav className="ml-auto">
           <Nav.Link href="/">
             <FaFileAlt />
--- src/pages/About.js
+++ src/pages/About.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Container, Row, Col, Image } from "react-bootstrap";
 import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
 
+import AboutNavbar from "../components/AboutNavbar";
+
 import zay from "../images/zay-headshot.jpg";
 import dan from "../images/dan-headshot.jpg";
 
@@ -13,11 +15,12 @@ function About() {
   return (
     <Container fluid={true} className="about__container">
       <Row>
+        <AboutNavbar />
+      </Row>
+      <Row>
         <Col>
           <br />
-          <br />
           <h3 className="text-center text-white">About the Devs</h3>
-          <br />
           <br />
         </Col>
       </Row>
@@ -31,7 +34,13 @@ function About() {
           md={{ order: 1 }}
           className="image__col"
         >
-          <Image src={zay} rounded />
+          <a
+            href="https://zaycation.dev/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image src={zay} rounded />
+          </a>
         </Col>
         <Col
           xs={{ span: 12, order: 2 }}
@@ -40,7 +49,13 @@ function About() {
           className="bio__col"
         >
           <div>
-            <h5 className="text-white text-center">zaycation.dev</h5>
+            <a
+              href="https://zaycation.dev/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h4 className="text-white text-center">zaycation.dev</h4>
+            </a>
             <p className="text-white text-center">
               I love designing user interfaces for mobile and desktop devices.
               I’m naturally inquisitive and tenacious. My project management
@@ -49,16 +64,33 @@ function About() {
             </p>
           </div>
           <div>
-            <FaGithub size={32} color="white" />
-            nbsp;
-            <FaTwitter size={32} color="white" />
-            nbsp;
-            <FaLinkedin size={32} color="white" />
+            <a
+              href="https://github.com/zaycation"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub size={32} color="white" />
+            </a>
+            <a
+              href="https://twitter.com/zaycationdev"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaTwitter size={32} color="white" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/isaiahthomas098/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin size={32} color="white" />
+            </a>
           </div>
         </Col>
       </Row>
 
       <br />
+      <hr />
       <br />
       <Row>
         <Col
@@ -68,7 +100,13 @@ function About() {
           className="bio__col"
         >
           <div>
-            <h5 className="text-white text-center">Dan Haas</h5>
+            <a
+              href="https://danzhaas.github.io/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h5 className="text-white text-center">Dan Haas</h5>
+            </a>
             <p className="text-white text-center">
               I am a full-stack web developer living in Baltimore, MD. I am an
               innovative problem-solver, experienced builder, lifelong learner,
@@ -77,11 +115,29 @@ function About() {
             </p>
           </div>
           <div>
-            <FaGithub size={32} color="white" />
-            nbsp;
-            <FaTwitter size={32} color="white" />
-            nbsp;
-            <FaLinkedin size={32} color="white" />
+            <a
+              href="https://github.com/danzhaas"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub size={32} color="white" />
+            </a>
+
+            <a
+              href="https://twitter.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaTwitter size={32} color="white" />
+            </a>
+
+            <a
+              href="https://www.linkedin.com/in/danzhaas/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin size={32} color="white" />
+            </a>
           </div>
         </Col>
         <Col
@@ -90,11 +146,18 @@ function About() {
           md={{ order: 2 }}
           className="image__col"
         >
-          <Image src={dan} rounded />
+          <a
+            href="https://danzhaas.github.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image src={dan} rounded />
+          </a>
         </Col>
       </Row>
 
       <br />
+      <hr />
       <br />
       <Row>
         <Col
@@ -103,7 +166,9 @@ function About() {
           md={{ order: 1 }}
           className="image__col"
         >
-          <Image src="https://via.placeholder.com/200" rounded />
+          <a href="/" target="_blank" rel="noopener noreferrer">
+            <Image src="https://via.placeholder.com/200" rounded />
+          </a>
         </Col>
         <Col
           xs={{ span: 12, order: 2 }}
@@ -112,18 +177,38 @@ function About() {
           className="bio__col"
         >
           <div>
-            <h5 className="text-white text-center">Dez Pringle</h5>
+            <a href="/" target="_blank" rel="noopener noreferrer">
+              <h5 className="text-white text-center">Dez Pringle</h5>
+            </a>
             <p className="text-white text-center">
               Lorem ipsum dolor sit amet, consectetur adip - | awaiting bio
               write up from Dez
             </p>
           </div>
           <div>
-            <FaGithub size={32} color="white" />
-            nbsp;
-            <FaTwitter size={32} color="white" />
-            nbsp;
-            <FaLinkedin size={32} color="white" />
+            <a
+              href="https://github.com/n305oul93"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub size={32} color="white" />
+            </a>
+
+            <a
+              href="https://twitter.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaTwitter size={32} color="white" />
+            </a>
+
+            <a
+              href="https://www.linkedin.com/in/dj-pringle/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin size={32} color="white" />
+            </a>
           </div>
         </Col>
       </Row>
