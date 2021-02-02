0966022a4ef598c5e3b3794ce07ec6cd9a81d4ca
D Pringle <n305oul93@gmail.com>
2021-02-01 19:04:52 -0600

    Updates styling for landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,132 +1,117 @@
 import React from 'react';
-import Header from '../components/Header';
-import { Button, ButtonGroup, Card, CardDeck, Navbar } from 'react-bootstrap';
-import Dez from '../images/Pringle_Desmond.jpg';
-import Dan from '../images/dan-headshot.jpg';
-import Zay from '../images/zay-headshot.jpg';
+import UniversalNavbar from '../components/UniversalNavbar';
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  ButtonGroup,
+  Card,
+  CardDeck,
+  Image
+} from 'react-bootstrap';
 import WalkingBook from '../images/walking-book.jpeg';
-import { FaLinkedin } from 'react-icons/fa';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
 import { IoMdConstruct } from 'react-icons/io';
 import './scss/LandingPage.css';
+
+import zay from '../images/zay-headshot.jpg';
+import dan from '../images/dan-headshot.jpg';
+import dez from '../images/dez-headshot.jpg';
 
 function LandingPage() {
   return (
     <div>
-      <Header />
-      {/* <Navbar bg='dark' variant='dark'>
-        <Navbar.Brand>Editor Name</Navbar.Brand>
-      </Navbar> */}
+      <Container fluid={true} className='hero__container'>
+        <Row>
+          <UniversalNavbar />
+        </Row>
+        <Row className='hero__row'>
+          <Col className='hero__img'>
+            <h1>Alien vs Editor</h1>
+            <h3>The open source editor for all</h3>
+            <ButtonGroup>
+              <Button pill variant='secondary' size='lg'>
+                Get Started
+              </Button>
+              <Button pill variant='secondary' size='lg'>
+                Change Theme
+              </Button>
+            </ButtonGroup>
+          </Col>
+        </Row>
+      </Container>
 
-      <div className='row m-3'>
-        <div className='col-md-5'>
-          <img src={WalkingBook} alt='walking book' />
-        </div>
-        <div className='col-md-5'>
-          <h1>Editor Name</h1>
-          <h3>The open source editor for all</h3>
-          <ButtonGroup>
-            <Button className='rounded-pill'>Get Sarted</Button>
-            <Button className='rounded-pill ml-2'>Change Theme</Button>
-          </ButtonGroup>
-        </div>
-      </div>
-      <div className='row m-3'>
-        <div className='col-md-5'>
-          <div className='w-75 m-auto border rounded bg-warning'>
-            <h2>Feature Peek</h2>
-            <div className='media'>
-              {/* <img src={IoMdConstruct} alt='' /> */}
-              <IoMdConstruct />
-              <div className='media-body'>
-                <h5>Built Using</h5>
-                <ul>
-                  <li>React</li>
-                  <li>Javascript</li>
-                </ul>
-              </div>
+      <Container fluid={true} className='landing__container'>
+        <Row>
+          <Col md={1}></Col>
+          <Col md={5}>
+            <div className='feature__peak'>
+              <h2>Feature Peek</h2>
             </div>
-          </div>
-        </div>
-        <div className='col-md-5'>
-          <p>
-            This project was built by a team of hard-working, talented
-            devs...Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad
-            dolores commodi reiciendis, libero nemo omnis ipsam nihil aliquam
-            quia molestiae.
-          </p>
-        </div>
-      </div>
-      <div className='row m-3'>
-        <div className='col-md-5'>
-          <h3>Who we are</h3>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Voluptatibus repellendus necessitatibus inventore exercitationem,
-            eligendi enim. Ipsam a veritatis sit sapiente?
-          </p>
-        </div>
-        <div className='col-md-5 d-flex'>
-          <CardDeck className='w-100 text-center'>
-            <Card className='p-3'>
-              <Card.Img
-                variant='top'
-                src={Zay}
-                className='h-50 rounded-circle'
-              />
-              <Card.Body>
-                <Card.Title>Zaycation</Card.Title>
-                {/* <Card.Text>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Similique nostrum ratione quia sequi nisi tempora?
-                </Card.Text> */}
-                <Button size='sm'>View Profile</Button>
-                <p>
-                  Linked
-                  <FaLinkedin />
-                </p>
-              </Card.Body>
-            </Card>
-            <Card className='p-3'>
-              <Card.Img
-                variant='top'
-                src={Dan}
-                className='h-50 rounded-circle'
-              />
-              <Card.Body>
-                <Card.Title>Dan Haas</Card.Title>
-                {/* <Card.Text>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Similique nostrum ratione quia sequi nisi tempora?
-                </Card.Text> */}
-                <Button size='sm'>View Profile</Button>
-                <p>
-                  Linked
-                  <FaLinkedin />
-                </p>
-              </Card.Body>
-            </Card>
-            <Card className='p-3'>
-              <Card.Img
-                variant='top'
-                src={Dez}
-                className='h-50 rounded-circle'
-              />
-              <Card.Body>
-                <Card.Title>Dez</Card.Title>
-                {/* <Card.Text>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Similique nostrum ratione quia sequi nisi tempora?
-                </Card.Text> */}
-                <Button size='sm'>View Profile</Button>
-                <p>
-                  Linked
-                  <FaLinkedin />
-                </p>
-              </Card.Body>
-            </Card>
-          </CardDeck>
-        </div>
-      </div>
+            {/* <img src={IoMdConstruct} alt='' /> */}
+            <div className='technologies'>
+              <div className='technologies__title'>
+                <IoMdConstruct size={24} />
+                <h5>Built Using</h5>
+              </div>
+              <p>ReactJS</p>
+              <p>NodeJS</p>
+              <p>Express</p>
+              <p>MongoDB</p>
+            </div>
+          </Col>
+          <Col md={5}>
+            <div>
+              <p className='tech__right'>
+                This project was built by a team of hard-working, talented
+                devs...Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Ad dolores commodi reiciendis, libero nemo omnis ipsam nihil
+                aliquam quia molestiae.
+              </p>
+            </div>
+          </Col>
+          <Col md={1}></Col>
+        </Row>
+        <Row>
+          <Col md={1}></Col>
+          <Col md={5}>
+            <h3>Who we are</h3>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Voluptatibus repellendus necessitatibus inventore exercitationem,
+              eligendi enim. Ipsam a veritatis sit sapiente?
+            </p>
+          </Col>
+          <Col md={5} className='card__col'>
+            <div className='card__left'>
+              <h4>zaycationdev</h4>
+              <Image src={'http://placehold.jp/100x100.png'} roundedCircle />
+              <FaGithub size={32} />
+              <Button block variant='success'>
+                View Profile
+              </Button>
+            </div>
+            <div className='card__middle'>
+              <h4>Dez Pringle</h4>
+              <Image src={'http://placehold.jp/100x100.png'} roundedCircle />
+              <FaGithub size={32} />
+              <Button block variant='success'>
+                View Profile
+              </Button>
+            </div>
+            <div className='card__right'>
+              <h4>Dan Haas</h4>
+              <Image src={'http://placehold.jp/100x100.png'} roundedCircle />
+              <FaGithub size={32} />
+              <Button block variant='success'>
+                View Profile
+              </Button>
+            </div>
+          </Col>
+          <Col md={1}></Col>
+        </Row>
+      </Container>
     </div>
   );
 }
